e15100c41cb19294f83cfa1334057e8704ad2479
ShadowXTE <111998018+ShadowXTE@users.noreply.github.com>
2022-08-31 13:57:51 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,13 +9,13 @@ node master.puppet.vm {
   file { '/root/README':
     ensure => file,
     content => $fqdn,
-    }
   }
+ }
   
   node /^web/ {
     include role::app_server
-  }
+ }
   
   node /^db/ {
     include role::db_server
-  }
+ }
