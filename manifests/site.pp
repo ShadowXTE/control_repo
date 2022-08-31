d99d6c8dcef9a3afade3d7ed6683b99d3354cf6c
ShadowXTE <111998018+ShadowXTE@users.noreply.github.com>
2022-08-30 19:12:11 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,9 @@
-node master.puppet.vm {
-  include role::master_server
-  }
-node default {
+node default {}
   file { '/root/README':
     ensure => file,
     content => 'This is a readme',
     owner   => 'root',
   }
-}
+node master.puppet.vm {
+  include role::master_server
+  }
