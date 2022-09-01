b6bd2e913c6b6354833e165061dc09c4b194bf4a
ShadowXTE <111998018+ShadowXTE@users.noreply.github.com>
2022-08-31 21:40:09 -0500

    Update init.pp

--- site/minecraft/manifest/init.pp
+++ site/minecraft/manifest/init.pp
@@ -1,20 +1,24 @@
 class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
-  }
+     }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
-   }
+     }
    package {'java':
     ensure => present,
-    }
+     }
     file {'/opt/minecraft/eula.txt':
       ensure => file,
       content => 'eula=true'
-    }
+     }
     file {'/etc/systemd/system/minecraft.service':
       ensure => file,
       source => 'puppet:///module/minecraft/minecraft_service',
      }
+    service {'minecraft':
+      ensure => running,
+      ensure => true,
+    }
 }
